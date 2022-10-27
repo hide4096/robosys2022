4b5a121ef73b5c8e44e74f933bf780fc38fe2ec5
hide4096 <asouhide2002@gmail.com>
2022-10-27 11:48:55 +0900

    [update]add license

--- test.bash
+++ test.bash
@@ -1,5 +1,17 @@
 #!/bin/bash
+# SPDX-FileCopyrightText: 2022 Aso Hidetoshi asouhide2002@gmaiil.com
+# SPDX-License-Identifier: BSD-3-Clause
 
-out=$(seq 10 | ./plus)
+ng () {
+    echo NG at Line $1
+    res=1
+}
 
-[ $out = 55.0 ]
+res=0
+
+### IO test ###
+out=$(seq 10 | ./plus) 
+[ "$out" = 55.0 ]|| ng ${LINENO}
+
+[ "$res" = 0 ] && echo OK
+exit $res
