69f22431d90bd095b04b88f826deaf0ce1640ab4
hide4096 <asouhide2002@gmail.com>
2022-10-27 11:53:01 +0900

    [update]テスト項目追加

--- test.bash
+++ test.bash
@@ -1,4 +1,4 @@
-#!/bin/bash -xv
+#!/bin/bash
 # SPDX-FileCopyrightText: 2022 Aso Hidetoshi asouhide2002@gmail.com
 # SPDX-License-Identifier: BSD-3-Clause
 
@@ -11,7 +11,11 @@ res=0
 
 ### IO test ###
 out=$(seq 10 | ./plus) 
-[ "$out" = 55.0 ]|| ng ${LINENO}
+[ "$out" = 55.0 ] || ng ${LINENO}
+
+out=$(echo あ | ./plus)
+[ "$?" = 1 ] || ng ${LINENO}
+[ "$out" = "" ] ||ng ${LINENO}
 
 [ "$res" = 0 ] && echo OK
 exit $res
